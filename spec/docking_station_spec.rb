b9d741fd683b3c9c15598e42bb2605388c54be53
Luke Phyall <lukeprospect@yahoo.com>
2019-02-05 16:56:19 +0000

    Adds bike working test.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,12 @@ require './lib/docking_station.rb'
 describe DockingStation do
    it {is_expected.to respond_to :release_bike}
 end
+
+describe DockingStation do
+  it {
+    station = DockingStation.new
+    bike = station.release_bike
+    expects bike.working?.to eq true
+  }
+
+end
